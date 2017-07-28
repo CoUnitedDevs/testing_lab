55cbe0d6524640af6685df46f16554c110d101a6
Arun <tummala.arun04@gmail.com>
2017-07-28 15:59:46 +0530

    minor change

--- app/models/stool_examinationreport.rb
+++ app/models/stool_examinationreport.rb
@@ -3,15 +3,10 @@ class StoolExaminationreport < ApplicationRecord
   has_many :tests, as: :testable
 
   after_save :set_test
-  # after_save :set_patient_test
 
   def set_test
     @test = tests.new(patient_id: self.patient_id)
     @test.save
   end
 
-  def set_patient_test
-    @patient = self.patient
-    @patient_test = @patient.patient_testurls.where(test_id: 1,test_url: 'STOOL EXAMINATION REPORT').update(test_id: self.id,status: 'active')
-  end
 end
